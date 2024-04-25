061361c58e49d2b7a9a3b669610a7509fdedef31
jekky25 <jekky25@list.ru>
2024-04-25 08:36:00 +0300

    remove param from the method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
 		return view('home');
     }
