8f642a3d48268f6337b88e192646aae97e519a3f
jekky25 <jekky25@list.ru>
2024-05-03 07:40:11 +0300

    add tests

---
+++ tests/Feature/ValidationFormSendTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Response;
+use Tests\TestCase;
+
+class ValidationFormSendTest extends TestCase
+{
+    /**
+     * Test status 200 ajax/send_mess.php 
+     */
+    public function test_send_valid_datas(): void
+    {
+        $this->post('ajax/send_mess.php', ['name' => 'Вася', 'e-mail' => 'jonny@list.ru', 'message' => 'текст текст текст'])
+        ->assertStatus(200);
+    }
+
+    /**
+     * Test form validation
+     */
+    public function test_send_no_valid_datas(): void
+    {
+        $response = $this->post('ajax/send_mess.php', ['name' => 'Вася'])
+        ->assertStatus(302);
+
+        $response = $this->post('ajax/send_mess.php', ['e-mail' => 'jonny@list.ru'])
+        ->assertStatus(302);
+
+        $response = $this->post('ajax/send_mess.php', ['name' => 'Вася', 'e-mail' => 'dgfhgdg', 'message' => 'текст текст текст'])
+        ->assertStatus(302);
+
+        $response = $this->post('ajax/send_mess.php', ['message' => 'текст текст текст'])
+        ->assertStatus(302);
+    }
+}
