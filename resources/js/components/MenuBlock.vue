c0822654988a4589a1def8b8ee15e3866cdaa84c
jekky25 <jekky25@list.ru>
2024-10-17 07:33:08 +0300

    install node.js install vue3. instead vue2 update vue menu

---
+++ resources/js/components/MenuBlock.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+	<ul><li v-for="name in names" :class="name.class"><a :href="name.href">{{name.n}}</a></li></ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "MenuBlock",
+		data() {
+				return {
+					names: 
+					[
+						{n:'Обо мне', href:'#about'}, 
+						{n:'Услуги', href:'#service'}, 
+						{n:'Стоимость', href:'#price'}, 
+						{n:'Контакты', href:'#contact'}, 
+						{n:'jekky25@list.ru', href:'mailto:jekky25@list.ru', class:'mail'}, 
+						{n:'jekky25', href:'skype:jekky25?call', class:'skype'}
+					]
+				};
+  			},
+		mounted() {}
+	}
+</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 	<meta name="yandex-verification" content="ddd70da113dd26d6" />
 	<meta name="description" content="Услуги Битрикс программиста. Оставьте заявку на разработку сайта на Битрикс.">
 	<meta property="og:description" content="Услуги Битрикс программиста. Оставьте заявку на разработку сайта на Битрикс." />
+	<link rel="stylesheet" href="{{ asset("css/app.css") }}" >
 	<link rel="stylesheet" href="{{ asset("css/bootstrap.min.css") }}" >
 	<link rel="stylesheet" href="{{ asset("css/bootstrap-select.min.css") }}">
 	<link rel="stylesheet" href="{{ asset("css/style.css?t=5") }}" >
@@ -19,6 +20,7 @@
 	<link rel="stylesheet" href="{{ asset("js/lightGallery.css") }}"/>
 	<link rel="shortcut icon" href="{{ asset("favicon.ico") }}" type="image/x-icon">
 	<link rel="icon" href="{{ asset("favicon.ico") }}" type="image/x-icon">
+	<script src="{{ asset("js/app.js") }}"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="{{ asset("js/bootstrap.min.js") }}"></script>
 	<script src="{{ asset("js/bootstrap-select.min.js") }}"></script>
@@ -27,7 +29,6 @@
 	<script src="{{ asset("js/owl.navigation.js") }}"></script>
 	<script src="{{ asset("js/jquery-ui.min.js") }}"></script>
 	<script type="text/javascript" src="{{ asset("js/highslide/highslide.js") }}"></script>
-	<script src="https://unpkg.com/vue@2.1.3/dist/vue.js"></script>
 	<script src="{{ asset("js/lightGallery.min.js") }}"></script>
 <script>
 	$(document).ready(function() {
@@ -35,7 +36,7 @@
 	});
 </script>
 </head>
-<body>
+<body id="app">
 		<header class="v-mod">
 			<section class="head-menu">
 				<menu-block class="head-menu-list"></menu-block>
@@ -55,25 +56,7 @@
 			<div class="logo-foot">Евгений Власов. Битрикс Разработка (с) 2018</div>
 		</section>
 	</footer>
-	<script>
-		Vue.component('menu-block', {
-			template: '<ul><li v-for="name in names" :class="name.class"><a :href="name.href">@{{name.n}}</a></li></ul>',
-			data: function () {
-				return {
-					names: [
-					{n:'Обо мне', href:'#about'}, 
-					{n:'Услуги', href:'#service'}, 
-					{n:'Стоимость', href:'#price'}, 
-					{n:'Контакты', href:'#contact'}, 
-					{n:'jekky25@list.ru', href:'mailto:jekky25@list.ru', class:'mail'}, 
-					{n:'jekky25', href:'skype:jekky25?call', class:'skype'}
-						]
-				};
-  			}
-		})
-		new Vue({ el: '.head-menu' });
-		new Vue({ el: '.v-mod-foot' });
-	</script>
+	<script src="{{ asset("js/app.js") }}"></script>
 	<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter50183023 = new Ya.Metrika2({ id:50183023, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/50183023" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 </body>
 </html>
